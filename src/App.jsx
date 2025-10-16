0057e4a7340c9cfa48e7ff49a881429229a2cb49
Chukwuwetalu Prayer Chibuike <chukwuwetaluprayet@gmail.com>
2025-10-16 15:53:38 -0700

    Linked my Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -42,26 +42,25 @@ function App() {
     <section id='home'>
           <nav>
       <ul>
-        <li><a href="">Home</a></li>
-        <li><a href="">About</a></li>
-        <li><a href="">Services</a></li>
-        <li className='logo'><a href="">King's</a></li>
-        <li><a href="">Gallery</a></li>
-        <li><a href="">Testimonial</a></li>
-        <li><a href="">Contact</a></li>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#services">Services</a></li>
+        <li className='logo'><a href="#home">King's</a></li>
+        <li><a href="#gallery">Gallery</a></li>
+        <li><a href="#testimonial">Testimonial</a></li>
+        <li><a href="#contact">Contact</a></li>
       </ul>
     </nav>
 
         <nav className='drop-down'>
           <label htmlFor="" className="logo">King's Clip</label>
       <ul ref={showRef}>
-        <li><a href="">Home</a></li>
-        <li><a href="">About</a></li>
-        <li><a href="">Services</a></li>
-        <li><a href="">Gallery</a></li>
-        <li><a href="">Faq</a></li>
-        <li><a href="">Testimonial</a></li>
-        <li><a href="">Contact</a></li>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#gallery">Gallery</a></li>
+        <li><a href="#testimonial">Testimonial</a></li>
+        <li><a href="#contact">Contact</a></li>
       </ul>
       <IoIosMenu onClick={showNav} className='menu'/>
     </nav>
